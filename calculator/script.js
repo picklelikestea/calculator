e57c925ca3e90c53508dbbcc3790bb290c4b5082
picklelikestea <gurungkhusi108@gmail.com>
2024-07-03 00:25:39 +0545

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,26 @@
+const display =document.querySelector(".display");
+const buttons =document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+//define function to calculate based on button clicked
+const calculate = (btnValue) => {
+    display.focus();
+    if(btnValue === "=" && output !== "") {
+        // if 
+        output = eval(output.replace("%", "/100"));
+    } else if(btnValue === "AC"){
+        output = "";
+    } else if(btnValue === "DEL"){
+    //if DEL
+    output = output.toString().slice(0, -1);
+    } else{
+if(output === "" && specialChars.includes(btnValue)) return;
+output += btnValue;
+    }
+display.value = output;
+};
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
